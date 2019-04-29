7007c062ea66520ad9ce26dffbba9f28d04d94b6
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-29 21:00:00 +0000

    Automatically backed up by Learn

--- src/containers/App.js
+++ src/containers/App.js
@@ -17,7 +17,7 @@ const App = (props) => {
         <Route exact path="/" component={Home} />
         <Route exact path="/movies" component={Movies} />
         <Route exact path="/directors" component={Directors} />
-        <Route exact path="/" component={Actors} />
+        <Route exact path="/actors" component={Actors} />
       </>
     </Router>
   );
