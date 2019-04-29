08b2c6b8c93b4462f0129060a48296498bf76cc5
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-29 21:01:16 +0000

    Automatically backed up by Learn

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,7 +13,7 @@ import Movies from '../components/Movies';
 const App = (props) => {
   return (
     <Router>
-      <>
+      <React.Fragment>
         <Route exact path="/" component={Home} />
         <Route exact path="/movies" component={Movies} />
         <Route exact path="/directors" component={Directors} />
