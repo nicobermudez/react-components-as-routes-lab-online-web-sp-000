da02ee116f3d47a55af0d90391577b41a4de6157
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-29 20:56:26 +0000

    Automatically backed up by Learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ const NavBar = () => {
       <NavLink to="/">Home</NavLink>
       <NavLink to="/movies">Movies</NavLink>
       <NavLink to="/directors">Directors</NavLink>
-      <NavLink to="/actors">Home</NavLink>
+      <NavLink to="/actors">Actors</NavLink>
     </div>
   );
 };
